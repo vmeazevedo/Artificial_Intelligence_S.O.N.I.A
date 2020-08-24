802e1df26ef015afc71349cf858981e54a01fc43
Gazebo <gabrielsirtori.correa@gmail.com>
2020-08-23 22:25:12 -0300

    Otimizado

--- S.O.N.I.A - v2.0.py
+++ S.O.N.I.A - v2.0.py
@@ -9,16 +9,43 @@ import sys
 from gtts import gTTS
 from playsound import playsound
 from requests_html import HTMLSession
+import pyttsx3
+from threading import Thread
 
 # CONFIGURAÇÕES MIC
 sr.Microphone(device_index=1)
 r=sr.Recognizer()
 r.energy_threshold = 5000
 
+# Utilizando a biblioteca pyttsx3 para a criação do audio, dessa forma, não é necessário buscar por arquivos de audio
+
+def speak(text):
+    speaker = pyttsx3.init('sapi5')
+    speaker.say(text)
+    speaker.runAndWait()
+
+# Criando um método para lidar apenas com o reconhecimento de voz
+def Recognition():
+    recognizer = sr.Recognizer()
+
+    try:
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)
+
+            print('Say:')
+
+            sound = recognizer.listen(source)
+            speech = recognizer.recognize_google(sound, language='pt').lower()
+            print('You said: ', speech)
+
+            return speech
+    except:
+        return ''
+
 def intro():
     # Função de inicialização do sistema
-	msg = "S.O.N.I.A - by: Vinicius Azevedo"
-	print("-" * len(msg) +  "\n{}\n".format(msg)  +   "-" * len(msg))
+	msg = 'S.O.N.I.A - by: Vinicius Azevedo'
+	print('-' * len(msg) +  '\n{}\n'.format(msg)  +   '-' * len(msg))
 
 def previsao(local=''):
     session = HTMLSession()
@@ -30,7 +57,7 @@ def previsao(local=''):
     state = r.html.find(selector_state, first=True).text
     selector_temp = '#wob_tm'
     temp = r.html.find(selector_temp, first=True).text
-    return("%s°C (%s)" %(temp, state))
+    return('%s°C (%s)' %(temp, state))
 
 local = 'São Paulo'
 if len(sys.argv)>0:
@@ -39,95 +66,66 @@ if len(sys.argv)>0:
 
 
 def cadastro_login():
-    with sr.Microphone() as source:
-        cria_audio("Informe seu usuário.")
-        print("Estou te ouvindo: ")
-        r.adjust_for_ambient_noise(source)
-        user_name = r.listen(source)
-        texto = r.recognize_google(user_name,language='pt-br')
-
-        try:    
-            with open('usuarios.txt') as f:
-                found = False
-                for line in f:
-                    if texto in line: 
-                        cria_audio("Bem vindo de vólta" + texto)
-                        found = True
-                        opções()
+    speak('Informe seu usuário.')
+    texto = Recognition()
+   
+    with open('usuarios.txt') as f:
+        found = False
+        for line in f:
+            if texto in line: 
+                speak('Bem vindo de vólta' + texto)
+                found = True
+                opções()
         
             if not found:
-                cria_audio('Não encontrei o seu usuário em meus registros.')
-                cria_audio("Lembrarei de você no seu próximo login.")
-                cria_audio("Por favor realize o login novamente.")
+                speak('Não encontrei o seu usuário em meus registros.')
+                speak('Lembrarei de você no seu próximo login.')
+                speak('Por favor realize o login novamente.')
                 with open('usuarios.txt', 'a') as f:
                     f.write(texto)
-                    f.write("\n")
+                    f.write('\n')
 
-        except:
-            print("Não entendi.")
+# Utilizando IN ao inves de == para que o comando seja acionado quando a palavra estiver na string, possibilitando
+# o reconhecimento do comando dentro de uma frase.
+# Utilizando apenas o RADICAL da palavra para a checagem, possibilitando que a palavra possa ser dita em diferentes
+# forma, ex: comando: Desligar Radical: Deslig Possibilidades: desligar, desligue, desligando...
 
 def opções():
     while True:
-        with sr.Microphone() as source:
-                print("Estou te ouvindo: ")
-                r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-
-                try:
-                    texto = r.recognize_google(audio,language='pt-br')
-                    if (texto == 'Google'):
-                        pesquisa_google()
-                    if (texto == 'pesquisar'):
-                        pesquisa_youtube()
-                    if (texto == 'desligar'):
-                        cria_audio("Desligando o sistema.")
-                        playsound('audio\\down.mp3')
-                        break
-                except:
-                    print("Não entendi.")
-                    
-                time.sleep(5)
-
-def cria_audio(resp):
-    #Funcao responsavel pela fala
-    date_string = datetime.datetime.now().strftime("%d%m%Y%H%M%S")
-    audio_file = "voice"+date_string+".mp3"
-    tts = gTTS(resp, lang='pt-br')
-    tts.save(audio_file)
-    playsound(audio_file)
+        texto = Recognition()
+        if ('google' in texto):
+            pesquisa_google()
+        if ('pesquis' in texto):
+            pesquisa_youtube()
+        if ('deslig' in texto):
+            speak('Desligando o sistema.')
+            playsound('audio\\down.mp3')
+            break    
+        time.sleep(5)
 
 # COMANDOS DE VOZ 
 def pesquisa_google():
     #Função responsável por pesquisar no Google
-    with sr.Microphone() as source:
-        resp = "\nO que gostaria?"
-        print("Aguardando a sua resposta...")
-        cria_audio(resp)
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source) 
-        try:
-            texto = r.recognize_google(audio,language='pt-br')
-            print("Você solicitou a pesquisa: {}".format(texto))
-            url='https://www.google.co.in/search?q='
-            search_url=url+texto
-            webbrowser.open(search_url)
-        except:
-            print("Não entendi.")
-
+    resp = '\nO que gostaria?'
+    speak(resp)
+    print('Aguardando a sua resposta...')
+    texto = Recognition()
+    print('Você solicitou a pesquisa: {}'.format(texto))
+    url='https://www.google.co.in/search?q='
+    search_url=url+texto
+    webbrowser.open(search_url)
+    
 def pesquisa_youtube():
     #Função responsável por pesquisar no Youtube
-    with sr.Microphone() as source:
-        resp = "\nO que gostaria?"
-        print("Aguardando a sua resposta...")
-        cria_audio(resp)
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-        texto = r.recognize_google(audio,language='pt-br')
-        print("Você solicitou a pesquisa: {}".format(texto))
-        kit.playonyt(texto)
+    resp = '\nO que gostaria?'
+    speak(resp)
+    print('Aguardando a sua resposta...')
+    texto = Recognition()
+    print('Você solicitou a pesquisa: {}'.format(texto))
+    kit.playonyt(texto)
 
 # ROTINA DO SISTEMA
 intro()
-cria_audio("Iniciândo o sistema. A previsão do tempo atual é: ")
-cria_audio(previsao(local))
+speak('Iniciando o sistema. A previsão do tempo atual é ')
+speak(previsao(local))
 cadastro_login()
